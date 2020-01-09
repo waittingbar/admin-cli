562676eee14e8bbe5fea422bf27908d6f6deb390
LiPengFei <demoworld@sina.com>
2020-01-09 11:07:09 +0800

    mysql

--- config/config.default.js
+++ config/config.default.js
@@ -57,13 +57,14 @@ module.exports = app => {
     // 单数据库信息配置
   client: {
     // host
-    host: '127.0.0.1',
+    // host: '127.0.0.1',
+    host: '192.168.0.97',
     // 端口号
     port: '3306',
     // 用户名
     user: 'root',
     // 密码
-    password: '928864229',
+    password: '928864229LPF!',
     // 数据库名
     database: 'eggdb',
   },
