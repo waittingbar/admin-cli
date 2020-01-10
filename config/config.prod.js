a3dfa4143190a14965c54e49297c7ba7f5e0296a
LiPengFei <demoworld@sina.com>
2020-01-10 09:34:33 +0800

    sql

--- config/config.prod.js
+++ config/config.prod.js
@@ -26,15 +26,6 @@ module.exports = app => {
   // 是否加载到 agent 上，默认关闭
   agent: false,
   }
-  const localIP = ip.address();
-  const domainWhiteList = [];
-  [7001, 9000, 9001].forEach(port => {
-    domainWhiteList.push(`http://localhost:${port}`);
-    domainWhiteList.push(`http://127.0.0.1:${port}`);
-    domainWhiteList.push(`http://${localIP}:${port}`);
-  });
-
-  exports.security = { domainWhiteList };
 
   return exports;
 };
