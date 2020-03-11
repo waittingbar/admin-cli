fb6021196899cd57d0985f0fdd2ab6d383555219
LiPengFei <demoworld@sina.com>
2020-03-11 13:38:30 +0800

    数据库地址修改

--- config/config.prod.js
+++ config/config.prod.js
@@ -11,13 +11,13 @@ module.exports = app => {
     // 单数据库信息配置
   client: {
     // host
-    host: '192.168.0.97',
+    host: '127.0.0.1',
     // 端口号
     port: '3306',
     // 用户名
     user: 'root',
     // 密码
-    password: '928864229LPF!',
+    password: '928864229Lpf!',
     // 数据库名
     database: 'eggdb',
   },
